858f539a2ecb743b19c32abfcb02dba3a60eddee
Ajayshetty97 <67274983+Ajayshetty97@users.noreply.github.com>
2020-07-16 01:56:54 +0530

    goal-4

--- RDIP/Codes/ALLJS.js
+++ RDIP/Codes/ALLJS.js
@@ -1,147 +1,153 @@
-var EnglishSen1 = [
-                             "John ate an apple before afternoon",	
-                             "before afternoon John ate an apple",	
-                             "John before afternoon ate an apple"
-];
-var EnglishSen2 = [
-                             "some students like to study in the night",
-                             "at night some students like to study"
-];
-var EnglishSen3 = [
-                             "John and Mary went to church",
-                             "Mary and John went to church"
-];
-var EnglishSen4 = [
-                            "John went to church after eating",	
-                            "after eating John went to church",
-                            "John after eating went to church"
-];
-var EnglishSen5 = [
-                             "did he go to market",	
-                             "he did go to market"
-];
-var EnglishSen6 = [
-                            "the woman who called my sister sells cosmetics",
-                            "the woman who sells cosmetics called my sister",
-                            "my sister who sells cosmetics called the woman",
-                            "my sister who called the woman sells cosmetics"
-];
-var EnglishSen7 = [
-                            "John goes to the library and studies",
-                            "John studies and goes to the library"
-];
-var EnglishSen8 = [
-                             "John ate an apple so did she",
-                             "she ate an apple so did John"
-];
-var EnglishSen9 = [
-                            "the teacher returned the book after she noticed the error",
-                            "the teacher noticed the error after she returned the book",
-                            "after the teacher returned the book she noticed the error",
-                            "after the teacher noticed the error she returned the book",
-                            "she returned the book after the teacher noticed the error",
-                            "she noticed the error after the teacher returned the book",
-                            "after she returned the book the teacher noticed the error",
-                            "after she noticed the error the teacher returned the book"
-];
-var EnglishSen10 = [
-                              "I told her that I bought a book yesterday",
-                              "I told her yesterday that I bought a book",
-                              "yesterday I told her that I bought a book",
-                              "I bought a book that I told her yesterday",
-                              "I bought a book yesterday that I told her",	
-                              "yesterday I bought a book that I told her"
+let englishSentences = [
+  [
+    'John ate an apple before afternoon',
+    'before afternoon John ate an apple',
+    'John before afternoon ate an apple'
+  ],
+  [
+    'some students like to study in the night',
+    'at night some students like to study'
+  ],
+  ['John and Mary went to church', 'Mary and John went to church'],
+  [
+    'John went to church after eating',
+    'after eating John went to church',
+    'John after eating went to church'
+  ],
+  ['did he go to market', 'he did go to market'],
+  [
+    'the woman who called my sister sells cosmetics',
+    'the woman who sells cosmetics called my sister',
+    'my sister who sells cosmetics called the woman',
+    'my sister who called the woman sells cosmetics'
+  ],
+  [
+    'John goes to the library and studies',
+    'John studies and goes to the library'
+  ],
+  ['John ate an apple so did she', 'she ate an apple so did John'],
+  [
+    'the teacher returned the book after she noticed the error',
+    'the teacher noticed the error after she returned the book',
+    'after the teacher returned the book she noticed the error',
+    'after the teacher noticed the error she returned the book',
+    'she returned the book after the teacher noticed the error',
+    'she noticed the error after the teacher returned the book',
+    'after she returned the book the teacher noticed the error',
+    'after she noticed the error the teacher returned the book'
+  ],
+  [
+    'I told her that I bought a book yesterday',
+    'I told her yesterday that I bought a book',
+    'yesterday I told her that I bought a book',
+    'I bought a book that I told her yesterday',
+    'I bought a book yesterday that I told her',
+    'yesterday I bought a book that I told her'
+  ]
 ];
 
+let hindiSentences = [
+  [
+    'राम और श्याम बाजार गयें',
+    'राम और श्याम गयें बाजार',
+    'बाजार गयें राम और श्याम',
+    'गयें बाजार राम और श्याम'
+  ],
+  [
+    'राम सोया और श्याम भी',
+    'श्याम सोया और राम भी',
+    'सोया श्याम और राम भी',
+    'सोया राम और श्याम भी'
+  ],
+  [
+    'मैंने उसे बताया कि राम सो रहा है',
+    'मैंने उसे बताया कि सो रहा है राम',
+    'उसे मैंने बताया कि राम सो रहा है',
+    'उसे मैंने बताया कि सो रहा है राम',
+    'मैंने बताया उसे कि राम सो रहा है',
+    'मैंने बताया उसे कि सो रहा है राम',
+    'उसे बताया मैंने कि राम सो रहा है',
+    'उसे बताया मैंने कि सो रहा है राम',
+    'बताया मैंने उसे कि राम सो रहा है',
+    'बताया मैंने उसे कि सो रहा है राम',
+    'बताया उसे मैंने कि राम सो रहा है',
+    'बताया उसे मैंने कि सो रहा है राम'
+  ],
+  [
+    'राम खाकर सोया',
+    'खाकर राम सोया',
+    'राम सोया खाकर',
+    'खाकर सोया राम',
+    'सोया राम खाकर',
+    'सोया खाकर राम'
+  ],
+  [
+    'बिल्लियों को मारकर कुत्ता सो गया',
+    'मारकर बिल्लियों को कुत्ता सो गया',
+    'बिल्लियों को मारकर सो गया कुत्ता',
+    'मारकर बिल्लियों को सो गया कुत्ता',
+    'कुत्ता सो गया बिल्लियों को मारकर',
+    'कुत्ता सो गया मारकर बिल्लियों को',
+    'सो गया कुत्ता बिल्लियों को मारकर',
+    'सो गया कुत्ता मारकर बिल्लियों को'
+  ],
+  [
+    'एक लाल किताब वहाँ है',
+    'एक लाल किताब है वहाँ',
+    'वहाँ है एक लाल किताब',
+    'है वहाँ एक लाल किताब'
+  ],
+  [
+    'एक बड़ी सी किताब वहाँ है',
+    'एक बड़ी सी किताब है वहाँ',
+    'बड़ी सी एक किताब वहाँ है',
+    'बड़ी सी एक किताब है वहाँ',
+    'वहाँ है एक बड़ी सी किताब',
+    'वहाँ है बड़ी सी एक किताब',
+    'है वहाँ एक बड़ी सी किताब',
+    'है वहाँ बड़ी सी एक किताब'
+  ]
+];
 
-
-var HindiSen1 = [
-                                "राम और श्याम बाजार गयें",
-                                 "राम और श्याम गयें बाजार",
-                                "बाजार गयें राम और श्याम",
-                                "गयें बाजार राम और श्याम"
-];
-var HindiSen2 = [
-                                  "राम सोया और श्याम भी",
-                                  "श्याम सोया और राम भी",
-                                  "सोया श्याम और राम भी",
-                                  "सोया राम और श्याम भी"
-];
-var HindiSen3 = [
-                                  "मैंने उसे बताया कि राम सो रहा है",
-                                  "मैंने उसे बताया कि सो रहा है राम",
-                                  "उसे मैंने बताया कि राम सो रहा है",
-                                  "उसे मैंने बताया कि सो रहा है राम",
-                                  "मैंने बताया उसे कि राम सो रहा है",
-                                  "मैंने बताया उसे कि सो रहा है राम",
-                                  "उसे बताया मैंने कि राम सो रहा है",
-                                  "उसे बताया मैंने कि सो रहा है राम",
-                                  "बताया मैंने उसे कि राम सो रहा है",
-                                  "बताया मैंने उसे कि सो रहा है राम",
-                                  "बताया उसे मैंने कि राम सो रहा है",
-                                  "बताया उसे मैंने कि सो रहा है राम"
-];
-var HindiSen4 = [
-                                 "राम खाकर सोया",
-                                  "खाकर राम सोया",
-                                  "राम सोया खाकर",
-                                  "खाकर सोया राम",
-                                  "सोया राम खाकर",
-                                  "सोया खाकर राम"
-];
-var HindiSen5 = [
-                                 "बिल्लियों को मारकर कुत्ता सो गया",	
-                                  "मारकर बिल्लियों को कुत्ता सो गया",	 
-                                  "बिल्लियों को मारकर सो गया कुत्ता",	
-                                  "मारकर बिल्लियों को सो गया कुत्ता",	
-                                  "कुत्ता सो गया बिल्लियों को मारकर",	
-                                 "कुत्ता सो गया मारकर बिल्लियों को",	
-                                  "सो गया कुत्ता बिल्लियों को मारकर",
-                                  "सो गया कुत्ता मारकर बिल्लियों को"
-];
-var HindiSen6 = [
-                                  "एक लाल किताब वहाँ है",
-                                   "एक लाल किताब है वहाँ",
-                                   "वहाँ है एक लाल किताब",
-                                    "है वहाँ एक लाल किताब"
-];
-var HindiSen7 = [
-                                     "एक बड़ी सी किताब वहाँ है",	
-                                     "एक बड़ी सी किताब है वहाँ",
-                                     "बड़ी सी एक किताब वहाँ है",
-                                     "बड़ी सी एक किताब है वहाँ",
-                                     "वहाँ है एक बड़ी सी किताब",
-                                     "वहाँ है बड़ी सी एक किताब",
-                                     "है वहाँ एक बड़ी सी किताब",
-                                      "है वहाँ बड़ी सी एक किताब"
-];
-
-function new()
-{
-    document.getElementById("description3").innerHTML= " "
-    count=0
-
-if(document.getElementById("Language1").selected)
-{
-     document.getElementById("description1").innerHTML="Form a sentence(Declarative or Interrogative or any other type)from the given words"
-     document.getElementById("description2").innerHTML="(select the buttons in proper order)"
-}
-else(document.getElementById("Language2").selected)
- {
-     document.getElementById("description1").innerHTML="Form a sentence(Declarative or Interrogative or any other type)from the given words"
-     document.getElementById("description2").innerHTML="(select the buttons in proper order)"
-     
-}
+function scramble() {
+    let jumbledDiplayDiv = document.getElementById('JumbledSentence');
+    while (jumbledDiplayDiv.firstChild) {
+    jumbledDiplayDiv.removeChild(jumbledDiplayDiv.firstChild);
+  }
+  document.getElementById('JumbledSentence').innerHTML = '';
+  document.getElementById('Evaluation').innerHTML = '';
+  document.getElementById('FormedSentence').innerHTML = '';
+  let language = document.getElementById('Language');
+  let selectedLanguage = language.options[language.selectedIndex].value;
+  if (selectedLanguage === 'English' || selectedLanguage === 'Hindi') {
+    document.getElementById('TextDescription').innerHTML =
+      'Form a sentence(Declarative or Interrogative or any other type)from the given words<br>(select the buttons in proper order)';
+    if (selectedLanguage === 'English') {
+      createSentence(englishSentences);
+    } else if (selectedLanguage === 'Hindi') {
+      createSentence(hindiSentences);
+    }
+  } else {
+    alert('Select language!');
+  }
 }
 
-
-
-
-
-
-                               
-
-
-
-    
-
+function createSentence(lauguageSentencesArray = [], statement = '') {
+  document.getElementById('JumbledSentence').innerHTML = '';
+  document.getElementById('Evaluation').innerHTML = '';
+  document.getElementById('FormedSentence').innerHTML = '';
+   let arrayIndex = Math.floor(Math.random() * lauguageSentencesArray.length);
+  let statemenrIndex = Math.floor(
+    Math.random() * lauguageSentencesArray[arrayIndex].length
+  );
+  if (!statement) {
+    statement = lauguageSentencesArray[arrayIndex][statemenrIndex];
+  }
+  let jumbledStatementArray = statement
+    .split(' ')
+    .sort(() => Math.random() - 0.5);
+  let jumbledDiplayDiv = document.getElementById('JumbledSentence');
+  let evaluationDiv = document.getElementById('Evaluation');
+  let formedSentence = document.getElementById('FormedSentence');
+  
+}
